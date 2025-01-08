e3b831d7a4e71eca3b9f85137d4a18a1386ca6ad
shinilv <19510296826@163.com>
2025-01-08 15:46:27 +0800

    Site updated: 2025-01-08 15:46:25

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1736131453046";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1736322380986";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
